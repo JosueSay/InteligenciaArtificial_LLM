cbd71e342c3c613751e6b34d98a22489c75c7619
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-10-01 17:25:52 -0600

    feat: implementación de langsmith en local vector database para rastreo de razonamiento

--- local_vector_database/lvd_langsmith.py
+++ local_vector_database/lvd_langsmith.py
@@ -1,56 +1,64 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, OpenAI
 from langchain_community.vectorstores import FAISS
-from langchain .chains.retrieval import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain import hub
+
+# Cargar variables de entorno
+load_dotenv()
 
 if __name__ == '__main__':
     print("Carga del archivo PDF...")
 
     # Cargar el pdf
     pdf_path = "D:\\UVG GitHub Repositorios\\Selectivo_IA\\local_vector_database\\2210.03629v3.pdf"
-
     loader = PyPDFLoader(file_path=pdf_path)
     documents = loader.load()
 
     print("Dividir el PDF en chunks...")
 
-    # Separar el documento
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separator = "\n")
-
-
-    docs = text_splitter.split_documents(documents = documents)
+    # Separar el documento en chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separator="\n")
+    docs = text_splitter.split_documents(documents=documents)
 
     print("Guardar los chunks en una base vectorial local...")
 
-    embeddings = OpenAIEmbeddings() # crear el objeto embbedings
-    vectorstore = FAISS.from_documents(docs, embeddings) # utiliza los chunks de docs para crear embeddings
-    vectorstore.save_local("faiss_index_react") # guardar los embeddings localmente en un formato
+    # Crear el objeto embeddings
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
 
-    # crear el agente para usar los embeddings
+    # Guardar los embeddings localmente
+    vectorstore.save_local("faiss_index_react")
 
     print("Cargar embedding de la base vectorial local...")
-    new_vectorstore = FAISS.load_local("faiss_index_react", embeddings, allow_dangerous_deserialization=True) # el último parámetro es una variable de seguridad cuando se carga información de manera local
+
+    # Cargar el índice FAISS guardado
+    new_vectorstore = FAISS.load_local("faiss_index_react", embeddings, allow_dangerous_deserialization=True)
 
     print("Creación y ejecución del agente...")
+
+    # Crear la cadena de recuperación
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     combine_docs_chain = create_stuff_documents_chain(
         OpenAI(),
         retrieval_qa_chat_prompt,
     )
 
+    # Crear la cadena de recuperación
     retrieval_chain = create_retrieval_chain(
         new_vectorstore.as_retriever(),
         combine_docs_chain,
     )
 
     print("Consulta...")
+
+    # Ejecutar la consulta
     res = retrieval_chain.invoke(
-        {"input":"Give me the gist of ReAct in 3 sentences"}
+        {"input": "Give me the gist of ReAct in 3 sentences"}
     )
 
     print(res["answer"])
